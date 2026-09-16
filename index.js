2d0e572048794838b395e37d4f5bf07689f071f5
Yishu Goyal <yishugoyalbth@gmail.com>
2026-09-16 08:45:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const axios = require("axios");
 
 const token = process.env.bot_token
 const id = process.env.bot_id
-const address = 'https://renuswa.netlify.app'
+const address = 'https://renuswa-06c126b081c4.herokuapp.com'
 
 const app = express();
 const appServer = http.createServer(app);
